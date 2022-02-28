d4fea4eee258f531f3f13949db1c2ab5fa725a3b
wwff1 <boss.lq71@mail.ru>
2022-02-28 16:24:54 +0500

    new

---
+++ src/App.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import { Button, Form, Container, Row, Col, ProgressBar } from 'react-bootstrap';
+import { generateImage } from './generate.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            uploadedImageURL: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=",
+            uploaded: false,
+            fp16: 0,
+            resize: "none",
+            generationStatus: 0,
+            updateGenerationProgressInterval: -1,
+            bytesUsed: 0
+        };
+    }
+
+    onUpload = (e) => {
+        var input = e.target;
+        var reader = new FileReader();
+        reader.onload = () => {
+            var dataURL = reader.result;
+            this.setState({
+                uploadedImageURL: dataURL,
+                uploaded: true
+            });
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+
+    generate = async () => {
+        if (this.state.generationStatus !== 0) {
+            return;
+        }
+
+        console.log(this.state);
+        if (this.state.uploaded === false) {
+            alert("Please upload an image.");
+            return;
+        }
+        if (this.state.resize === "none") {
+            alert("Please select a resize method.");
+            return;
+        }
+        
+        window.progress = 0;
+        window.bytesUsed = 0;
+        let updateGenerationProgressInterval = setInterval(() => {
+            this.setState({
+                generationProgress: window.progress * 100,
+                bytesUsed: window.bytesUsed
+            });
+
+            if (this.state.generationStatus !== 1) {
+                clearInterval(updateGenerationProgressInterval);
+            }
+        }, 500);
+
+
+        this.setState({
+            generationStatus: 1,
+            updateGenerationProgressInterval: updateGenerationProgressInterval
+        });
+        let success = false;
+        try {
+            await generateImage(this.state.resize, this.state.fp16, "uploaded-image", "output");
+            success = true;
+        } catch (error) {
+            alert("Error encountered while generating image: " + error);
+            this.setState({
+                generationStatus: 0
+            });
+        }
+
+        if (success) {
+            this.setState({
+                generationStatus: 2
+            });
+        }
+        
+    }
+    
+    componentWillUnmount = () => {
+        if (this.state.updateGenerationProgressInterval !== -1) {
+            clearInterval(this.state.updateGenerationProgressInterval);
+        }
+    }
+    
+    render () {
+        return (
+            <div className="app">
+                <Container fluid style={{"display": this.state.generationStatus === 0 ? "block" : "none"}}>
+
+                    <Row className="margin">
+                        <Col/>
+                        <Col xs="12" md="8" lg="6">
+                            <Form>
+                                <Form.File accept="image/*" label={(this.state.uploaded ? "Выберите изображение" : "Загрузить изображение")} onChange={this.onUpload} multiple={false} custom />
+                            </Form>
+                            
+                        </Col>
+                        <Col/>
+                    </Row>
+                    <Row className="margin">
+                        <Col/>
+                        <Col xs="12" md="8" lg="5" xl="4" style={{textAlign: "center", margin: "20px"}}>
+                            <img id="uploaded-image" alt="" src={this.state.uploadedImageURL} />
+                        </Col>
+                        <Col/>
+                    </Row>
+                    <Row className="margin">
+                        <Col/>
+                        <Col xs="12" md="8" lg="6" style={{textAlign: "center"}}>
+                            <Form>
+                                <Form.Group controlId="resize">
+                                    <Form.Control defaultValue="none" as="select" onChange={(e) => this.setState({resize: e.target.value})}>
+                                        <option value="none" disabled>Выберите размер сгенерированного изображения</option>
+                                        {/*<option value="s">Small (Fast)</option>*/}
+                                        {/*<option value="m">Medium</option>*/}
+                                        <option value="l">Стандартное</option>
+                                        <option value="original">Не изменять размер (Не обрабатывает слишком большие разрешения)</option>
+                                    </Form.Control>
+                                </Form.Group>
+                                {/*<Form.Group controlId="fp16">*/}
+                                {/*    <Form.Control as="select" onChange={(e) => this.setState({fp16: parseInt(e.target.value)})}>*/}
+                                {/*        <option value="0">Force FP16 For Speed (Lower Quality): No</option>*/}
+                                {/*        <option value="1">Force FP16 For Speed (Lower Quality): Yes</option>*/}
+                                {/*    </Form.Control>*/}
+                                {/*</Form.Group>*/}
+                                <Button variant="primary" onClick={this.generate}>Сгенерировать</Button>
+                            </Form>
+                        </Col>
+                        <Col/>
+                    </Row>
+                </Container>
+
+                <div className="overlay" style={{"display": this.state.generationStatus === 1 ? "block" : "none"}}>
+                
+                    <div style={{"marginTop":"calc( 50vh - 50px )", "height": "100px", "textAlign": "center"}}>
+                        <Container fluid>
+                            <Row>
+                                <Col/>
+                                <Col xs="12" md="8" lg="6" style={{textAlign: "center"}}>
+                                    <ProgressBar now={this.state.generationProgress} style={{"margin": "10px"}} />
+                                    <p>Generating image...</p>
+                                    <p>This may take 15 to 30 seconds depending on your device.</p>
+                                    <p>Memory usage (MB): {this.state.bytesUsed / 1000000} </p>
+                                </Col>
+                                <Col/>
+                            </Row>
+                        </Container>
+                    </div>
+                    
+                </div>
+
+                <div className="overlay" style={{"display": this.state.generationStatus === 2 ? "block" : "none"}}>
+                    <Container fluid>
+                        <Row className="margin">
+                            <Col/>
+                            <Col xs="12" md="8" lg="5" xl="4" style={{textAlign: "center", margin: "20px"}}>
+                                <canvas id="output"></canvas>
+                            </Col>
+                            <Col/>
+                        </Row>
+                        <Row className="margin">
+                            <Col/>
+                            <Col xs="12" md="12" lg="12" xl="10" style={{textAlign: "center", margin: "20px"}}>
+                                <Button variant="primary" onClick={() => window.location.reload()}>Restart</Button>
+                            </Col>
+                            <Col/>
+                        </Row>
+                    </Container>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/generate.js
@@ -0,0 +1,134 @@
+import * as tf from '@tensorflow/tfjs';
+import * as tfc from '@tensorflow/tfjs-converter';
+
+window.tf = tf;
+window.tfc = tfc;
+window.progress = 0;
+window.bytesUsed = 0;
+
+tf.enableProdMode();
+
+let start;
+
+const MODEL_URL = window.location.href + 'model_full/model.json';
+
+function mirrorPadFunc(input, pad_arr) {
+    return tf.tidy(() => {
+        for (let i = 0; i < 4; i++) {
+            if (pad_arr[i][0] !== 0 || pad_arr[i][1] !== 0) {
+                let slice_size = [-1, -1, -1, -1];
+                slice_size[i] = pad_arr[i][0];
+                let slice_begin = [0, 0, 0, 0];
+
+                let padding_left = input.slice(slice_begin, slice_size);
+
+                slice_size = [-1, -1, -1, -1];
+                slice_size[i] = pad_arr[i][1];
+                slice_begin = [0, 0, 0, 0];
+                slice_begin[i] = input.shape[i] - pad_arr[i][1];
+
+                let padding_right = input.slice(slice_begin, slice_size);
+                
+                input = tf.concat([padding_left, input, padding_right], i);
+            }
+
+            if (pad_arr[i][0] > 1 || pad_arr[i][1] > 1) {
+                return
+            }
+        }
+        return input;
+    });
+}
+
+window.mirrorPadFunc = mirrorPadFunc;
+
+const progressesList = [0.00023367749587460492, 0.054088046653978504, 0.1804816724673639, 0.18052037621199904, 0.2528568019649621, 0.37458444400475477, 0.39315031021211105, 0.39319017797911254, 0.4444196766347441, 0.5207431700988491, 0.550593651422125, 0.5542242372745627, 0.5605664132978859, 0.5806242652109398, 0.5927784050567816, 0.5962346785553008, 0.5981026434950807, 0.5989430676647844, 0.6435568450337933, 0.6676838282371483, 0.6684442258671517, 0.7463103400111626, 0.9019785470675509, 0.95];
+let num_called = 0;
+
+const mirrorPad = async (node) => {
+    let progress = 0.9 * (performance.now() - start)/(15463.61999999499);
+
+    if (num_called >= progressesList.length) {
+        progress = 0.95;
+    } else {
+        progress = progressesList[num_called];
+    }
+    num_called += 1;
+
+    window.progress = progress;
+
+    let memoryInfo = tf.memory();
+    window.bytesUsed = memoryInfo.numBytes;
+
+    await tf.nextFrame();
+
+    if (node.attrs.mode !== "reflect") {
+        return
+    }
+    let pad_tensor = node.inputs[1];
+    if (node.inputs[0].shape.length === 4) {
+        let pad_arr = await pad_tensor.array();
+        let input = node.inputs[0];
+        return mirrorPadFunc(input, pad_arr);
+    } else {
+        return
+    }
+};
+
+tfc.registerOp('MirrorPad', mirrorPad);
+
+const generate = async (model, long_side_scale_size, img, output) => {
+    let img_tensor = tf.browser.fromPixels(img);
+    let scaled_img_tensor;
+    if (long_side_scale_size !== -1) {
+        let scale_factor = (img_tensor.shape[0] > img_tensor.shape[1] ? img_tensor.shape[0] : img_tensor.shape[1]) / long_side_scale_size;
+        let scaled_size = [Math.round(img_tensor.shape[0] / scale_factor), Math.round(img_tensor.shape[1] / scale_factor)];
+        scaled_img_tensor = tf.tidy(() => (
+            tf.image.resizeBilinear(img_tensor, scaled_size).expandDims(0).div(255)
+        ));
+        img_tensor.dispose();
+    } else {
+        scaled_img_tensor = tf.tidy(() => (
+            img_tensor.expandDims(0).div(255)
+        ));
+        img_tensor.dispose();
+    }
+    
+    start = performance.now();
+    let generated = await model.executeAsync({'test': scaled_img_tensor});
+    scaled_img_tensor.dispose();
+    let end = performance.now();
+
+    tf.browser.toPixels((generated.squeeze(0).add(1)).div(2), output);
+    generated.dispose();
+}
+
+let preHeat = () => {
+    let model_load_start = performance.now();
+    tfc.loadGraphModel(MODEL_URL).then((model) => {
+        let model_load_end = performance.now();
+        model.dispose();
+    });
+}
+
+let generateImage = async (resize, fp16, img_id, canvas_id) => {
+        tf.env().set('WEBGL_FORCE_F16_TEXTURES', false);
+
+    let long_side_scale_size;
+
+    if (resize === "l") {
+        long_side_scale_size = 500;
+    } else {
+        long_side_scale_size = -1;
+    }
+
+    let model_load_start = performance.now();
+    await tfc.loadGraphModel(MODEL_URL).then(async (model) => {
+        let model_load_end = performance.now();
+        await generate(model, long_side_scale_size, document.getElementById(img_id), document.getElementById(canvas_id));
+        tf.disposeVariables();
+    });
+    window.progress = 1.0;
+};
+
+export {preHeat, generateImage};
