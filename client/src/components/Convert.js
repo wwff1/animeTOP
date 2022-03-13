290fc1f5edd8acce5535973dd105aad372aae632
Qusado <73778368+Qusado@users.noreply.github.com>
2022-03-13 21:45:30 +0500

    convertation

--- client/src/components/Convert.js
+++ client/src/components/Convert.js
@@ -1,16 +1,93 @@
 import React, {useState} from 'react'
-import {Link} from "react-router-dom";
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import fileDownload from 'js-file-download'
+
+
+
 export const Convert =  ({image}) => {
     const [uploadedImage, setUploadedImage] = useState();
+    const [uploadedImageURL, setUploadedImageURL] = useState("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=")
+    const [uploaded, setUploaded] = useState(false)
+    const [resize, setResize] = useState("none")
 
-console.log(image);
-if(image!==undefined)
-    console.log(image);
+    function onUpload(e) {
+        var input = e.target;
+        var reader = new FileReader();
+        reader.onload = () => {
+            var dataURL = reader.result;
+            setUploadedImageURL(dataURL)
+            setUploaded(true)
+        };
+        reader.readAsDataURL(input.files[0]);
+
+        var fileList = e.target.files;
+        setUploadedImage(fileList);
+    }
+
+    function convert(){
+        if (uploaded === false) {
+            alert("Please upload an image.");
+            return;
+        }
+        if (resize === "none") {
+            alert("Please select a resize method.");
+            return;
+        }
+
+        if(resize === "p")
+        {
+            let blob = uploadedImage[0].slice(0, uploadedImage[0].size, 'image/png');
+            let newFile = new File([blob], 'convert_to_png.png', {type: 'image/png'});
+            fileDownload(newFile, newFile.name)
+        }
+        else if(resize === "j")
+        {
+            let blob = uploadedImage[0].slice(0, uploadedImage[0].size, 'image/png');
+            let newFile = new File([blob], 'convert_to_jpg.jpg', {type: 'image/jpg'});
+            fileDownload(newFile, newFile.name)
+        }
+    }
+
     return(
        <>
-        <h1>slnerigernlverlnvre</h1>
-       </>
+           <div className="app" id="lol">
+               <Container>
 
+                   <Row className="margin">
+                       <Col/>
+                       <Col xs="12" md="8" lg="6">
+                           <Form>
+                               <Form.File accept="image/*" label={"Выберите изображение"} onChange={onUpload} multiple={false} custom />
+                           </Form>
+                       </Col>
+                       <Col/>
+                   </Row>
+                   <Row className="margin">
+                       <Col/>
+                       <Col xs="12" md="8" lg="5" xl="4" style={{textAlign: "center", margin: "20px"}}>
+                           <img id="uploaded-image" alt="" src={uploadedImageURL} />
+                       </Col>
+                       <Col/>
+                   </Row>
+                   <Row className="margin">
+                       <Col/>
+                       <Col xs="12" md="8" lg="6" style={{textAlign: "center"}}>
+                           <Form>
+                               <Form.Group controlId="resize">
+                                   <Form.Control defaultValue="none" as="select" onChange={(e) => setResize(e.target.value)}>
+                                       <option value="none" disabled>Выберите</option>
+                                       <option value="p">png</option>
+                                       <option value="j">jpg</option>
+                                   </Form.Control>
+                               </Form.Group>
+                               <Button variant="primary" onClick={convert}>Конверировать</Button>
+                           </Form>
+                       </Col>
+                       <Col/>
+                   </Row>
+               </Container>
+           </div>
+        </>
     )
 
 }
