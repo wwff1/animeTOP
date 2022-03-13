7c512077104cf13ec78146f897679c0389a3a2f2
wwff1 <boss.lq71@mail.ru>
2022-03-13 14:16:54 +0500

    new_save

--- server/controllers/images.controller.js
+++ server/controllers/images.controller.js
@@ -2,17 +2,18 @@ var fs = require('fs');
 
 exports.add = async (req, res) =>{
     const dir = './static';
-    let filename = '';
-
+    var filename = '';
     fs.readdir(dir, (err, files) => {
         filename = `image${files.length}.png`
-        console.log(files.length);
+        copy()
+        setTimeout(delet, 2000)
     });
-    console.log(filename)
-    const path = `F:/anime/animeTOP/server/static/${filename}`
-    console.log(path)
-    fs.copyFile(`C:/Users/Димас/Downloads/image.png`, path, err => { if(err) throw err; // не удалось переместить файл
-        console.log('Файл успешно перемещён1')})
-    fs.unlink('C:/Users/Димас/Downloads/image.png', err => { if(err) throw err; // не удалось переместить файл
+    function copy(){
+        fs.copyFile(`C:/Users/Димас/Downloads/image.png`, `F:/anime/animeTOP/server/static/${filename}`, err => { if(err) throw err; // не удалось переместить файл
+            console.log('Файл успешно перемещён1')})
+    }
+    function delet(){
+        fs.unlink('C:/Users/Димас/Downloads/image.png', err => { if(err) throw err; // не удалось переместить файл
             console.log('Файл успешно перемещён2')})
+    }
 }
